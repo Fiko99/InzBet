4914585b84825bad113cfb38a9f6eeb73afc599b
Fiko99 <mikolaj.legierski99@gmail.com>
2021-12-08 17:35:59 +0100

    Small changes

--- app/src/main/java/com/example/inzbet/fragments/MatchesFragment.java
+++ app/src/main/java/com/example/inzbet/fragments/MatchesFragment.java
@@ -2,6 +2,7 @@ package com.example.inzbet.fragments;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,66 +33,20 @@ public class MatchesFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_account, container, false);
+        View view = inflater.inflate(R.layout.fragment_matches, container, false);
         new HttpGetRequest(this).execute();
-        //downloadData();
         textView = view.findViewById(R.id.textView);
         textView.setText(match.getMatchday().toString());
 
-//        ExecutorService executor = Executors.newSingleThreadExecutor();
-//        executor.execute(new Runnable() {
-//            @Override
-//            public void run() {
-//                HttpURLConnection con = null;
-//                try {
-//                    URL url = new URL("https://api.football-data.org/v2/matches");
-//                    con = (HttpURLConnection) url.openConnection();
-//                    con.setRequestProperty("Accept", "application/json");
-//                    con.setRequestProperty("X-Auth-Token", "f6765dcdd1024189a9d257a09fe451c8");
-//                    con.getHeaderField("Accept");
-//                    con.getHeaderField("X-Auth-Token");
-//                    if (con.getResponseCode() == 200) {
-//                        BufferedReader is = new BufferedReader(new InputStreamReader(con.getInputStream()));
-//                        Gson gson = new Gson();
-//                        match = gson.fromJson(is, Match.class);
-//                        con.disconnect();
-//                    }
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }); {
-//
-//        }
-
         return view;
     }
-
-//    private void downloadData() {
-//        HttpURLConnection con = null;
-//        try {
-//            URL url = new URL("https://api.football-data.org/v2/matches");
-//            con = (HttpURLConnection) url.openConnection();
-//            con.setRequestMethod("GET");
-//            con.setRequestProperty("Accept", "application/json");
-//            con.setRequestProperty("X-Auth-Token", "f6765dcdd1024189a9d257a09fe451c8");
-//            if (con.getResponseCode() == 200) {
-//                BufferedReader is = new BufferedReader(new InputStreamReader(con.getInputStream()));
-//                Gson gson = new Gson();
-//                match = gson.fromJson(is, Match.class);
-//                con.disconnect();
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-
 }
 
 class HttpGetRequest extends AsyncTask<Void, Void, Void> {
 
     Fragment fragment;
     Match match;
+    String apiToken = "f6765dcdd1024189a9d257a09fe451c8";
 
     public HttpGetRequest(Fragment fragment) {
         this.fragment = fragment;
@@ -104,16 +59,15 @@ class HttpGetRequest extends AsyncTask<Void, Void, Void> {
             URL url = new URL("https://api.football-data.org/v2/matches");
             con = (HttpURLConnection) url.openConnection();
             con.setRequestProperty("Accept", "application/json");
-            con.setRequestProperty("X-Auth-Token", "f6765dcdd1024189a9d257a09fe451c8");
-            System.out.println(con.getRequestProperty("X-Auth-Token"));
-//            con.getHeaderField("Accept");
-//            con.getHeaderField("X-Auth-Token");
+            con.setRequestProperty("X-Auth-Token", apiToken);
             if (con.getResponseCode() == 200) {
                 BufferedReader is = new BufferedReader(new InputStreamReader(con.getInputStream()));
                 Gson gson = new Gson();
                 match = gson.fromJson(is, Match.class);
                 con.disconnect();
             }
+            Log.e("Info", con.getRequestProperty("X-Auth-Token"));
+            Log.e("Info", match.toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
